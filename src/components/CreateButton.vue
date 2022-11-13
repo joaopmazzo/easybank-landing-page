94a767815a8e9aa32f5b954d3d5173925b965adc
João Paulo Mazzo <mazzojp@gmail.com>
2022-11-13 03:04:49 -0300

    create button component

---
+++ src/components/CreateButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button type="button">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style scoped>
+button {
+  border-style: none;
+  border-radius: 22px;
+
+  padding: 14px 34px;
+
+  background-image: var(--green-gradient);
+
+  color: white;
+  font-weight: var(--bold);
+}
+</style>
