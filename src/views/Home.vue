793ce4c0712734a9ef280ec13ade6ec721210bc8
João Paulo Mazzo <mazzojp@gmail.com>
2022-11-13 03:06:42 -0300

    create home view

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <header-component />
+  <intro-section />
+  <!-- <div id="container-wrapper"> -->
+  <benefits-sections />
+  <!-- </div> -->
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+import Intro from "../components/Intro.vue";
+import Benefits from "../components/Benefits.vue";
+
+export default {
+  name: "Home",
+  components: {
+    "header-component": Header,
+    "intro-section": Intro,
+    "benefits-sections": Benefits,
+  },
+};
+</script>
+
+<style>
+/* #container-wrapper {
+  width: 90%;
+  margin: 0 auto;
+} */
+</style>
