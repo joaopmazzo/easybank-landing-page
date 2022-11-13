780054470c4fa58bdce721d07f275b22d3f575bc
João Paulo Mazzo <mazzojp@gmail.com>
2022-11-13 03:03:24 -0300

    create navigation with mobile style

---
+++ src/components/CreateNavigation.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="overlay">
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Contact</a>
+      <a href="#">Blog</a>
+      <a href="#">Careers</a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreateNavigation",
+};
+</script>
+
+<style scoped>
+.overlay {
+  width: 100vw;
+  height: calc(100vh - 64px);
+
+  background: var(--overlay);
+}
+
+nav {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  gap: 1.5rem;
+  padding: 2rem 0;
+  margin: 1.5rem;
+
+  background: white;
+  border-radius: 4px;
+
+  font-size: var(--navitem-mobile);
+}
+</style>
