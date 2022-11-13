9f1478bcba741ce7770e018bc3c077ba5824a2f0
João Paulo Mazzo <mazzojp@gmail.com>
2022-11-13 03:04:27 -0300

    create the benefits items with mobile style

---
+++ src/components/CreateBenefits.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <img :src="image" :alt="imageSrc" />
+    <h2>{{ title }}</h2>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreateBenefits",
+  props: {
+    image: String,
+    imageSrc: String,
+    title: String,
+    text: String,
+  },
+};
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.25rem;
+
+  text-align: center;
+}
+
+h2 {
+  color: var(--dark-blue);
+  font-size: var(--topic-small);
+  font-weight: var(--light);
+}
+
+p {
+  color: var(--grayish-blue);
+}
+</style>
